1bd19a26dff88162ed181d4534e89ae498f614e8
Paul Barrick <peb7268@gmail.com>
2019-02-14 18:14:04 -0500

    wip

--- deploy.php
+++ deploy.php
@@ -8,8 +8,18 @@ host('162.241.253.84')
 ->stage('production')
 ->user('primepa6', 'user is {{user}}')
 ->set('http_user', 'primepa6')
+->set('keep_releases', '2')
 ->set('deploy_path', '/home2/primepa6/public_html')
 ->set('repository', 'https://github.com/imperative-design/primepainters.git');
+
+task('node:install', function(){
+    run('wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash');
+    run('source ~/.bash_profile');
+    run('nvm install stable');
+    run('cd current/wp-content/themes/primepainters');
+    run('npm install');
+});
+
 task('deploy', [
     'deploy:info',
     'deploy:prepare',
@@ -23,13 +33,15 @@ task('deploy', [
     'deploy:unlock',
     'cleanup',
 ])->desc('Deploy your project');
-
+// after('deploy', 'node:install');
+// after('node:install', 'success');
 after('deploy', 'success');
+
 
 
 /**
  * Installing Node on Bluehost
- * wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash
+ * 
  * source ~/.bash_profile
  * nvm install stable
  * 
